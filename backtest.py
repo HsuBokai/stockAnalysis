e876830d5f10a0b2af8f1296a1c5a2133c5fbe63
HsuBokai <exp2.718281@gmail.com>
2019-09-01 11:47:08 +0800

    3 month income v001

--- backtest.py
+++ backtest.py
@@ -4,27 +4,27 @@ from twstock import Stock
 from functools import partial
 import numpy as np
 
-STOCKS = [ \
-	'2801', \
-	'2809', \
-	'2812', \
-	'2834', \
-	'2836', \
-	'2845', \
-	'2849', \
-	'2880', \
-	'2883', \
-	'2884', \
-	'2885', \
-	'2886', \
-	'2888', \
-	'2889', \
-	'2890', \
-	'2892', \
-	'5820', \
-	#'5876', \
-	'5880', \
-	]
+STOCKS = { \
+	'2801' : '彰銀', \
+	'2809' : '京城銀', \
+	'2812' : '台中銀', \
+	'2834' : '台企銀', \
+	'2836' : '高雄銀', \
+	'2845' : '遠東銀', \
+	'2849' : '安泰銀', \
+	'2880' : '華南金', \
+	'2883' : '開發金', \
+	'2884' : '玉山金', \
+	'2885' : '元大金', \
+	'2886' : '兆豐金', \
+	'2888' : '新光金', \
+	'2889' : '國票金', \
+	'2890' : '永豐金', \
+	'2892' : '第一金', \
+	'5820' : '日盛金', \
+	#'5876' : '上海商銀', \
+	'5880' : '合庫金', \
+	}
 
 YEAR_COLUMN_MAP = { \
 	2018: 0, \
@@ -33,23 +33,45 @@ YEAR_COLUMN_MAP = { \
 	2015: 36, \
 	}
 
+def decide(history):
+	#print(history)
+	yoy = [ np.sum(h[0:3]) / np.sum(h[12:15]) for h in history ]
+	#print(yoy)
+	yoy_dict = dict(zip(STOCKS.keys(), yoy))
+	#print(yoy_dict)
+	advise = [ k for k in sorted(yoy_dict, key=yoy_dict.get, reverse=True) ]
+	#print(advise)
+	#print([ yoy_dict[k] for k in sorted(yoy_dict, key=yoy_dict.get, reverse=True) ])
+	return advise[0]
+
+def analyze(year):
+	my_load = partial(np.loadtxt, dtype=np.str, delimiter=',')
+	sale_file = [ './' + s + '/SaleMonDetail.csv' for s in STOCKS.keys() ]
+	sale_list = [ my_load(f)[:0:-1, 7].astype(np.float) for f in sale_file ]
+	#sale_dict = dict(zip(STOCKS.keys(), sale_list))
+	#print(sale_dict)
+	end_month = YEAR_COLUMN_MAP[year]
+	start_month = end_month + 10
+	return [ decide([ l[0-m::-1] for l in sale_list ]) for m in range(start_month, end_month, -1) ]
+
 def backtest(year):
 	end = YEAR_COLUMN_MAP[year]
 	start = end + 10
 	my_load = partial(np.loadtxt, dtype=np.str, delimiter=',')
-	price_file = [ './' + s + '/price.csv' for s in STOCKS ]
+	price_file = [ './' + s + '/price.csv' for s in STOCKS.keys() ]
 	price_list = [ my_load(f)[start:end:-1, 2].astype(np.float) for f in price_file ]
-	price_dict = dict(zip(STOCKS, price_list))
+	price_dict = dict(zip(STOCKS.keys(), price_list))
 	#print(price_dict)
-	return_list = [ np.reciprocal(price_dict[s]) * price_dict[s][-1] for s in STOCKS ]
+	return_list = [ np.reciprocal(price_dict[s]) * price_dict[s][-1] for s in STOCKS.keys() ]
 	#print(return_list)
 	AVG = np.mean(return_list)
-	return_dict = dict(zip(STOCKS, return_list))
+	return_dict = dict(zip(STOCKS.keys(), return_list))
 	#print(return_dict)
-	invest = ['2880','2888','2801']
+	invest = analyze(year)
 	INV = np.mean([ return_dict[stock_id][idx] for idx, stock_id in enumerate(invest) ])
 	print('=============== ' + str(year) + ' ===============')
 	print('AVG ReturnOfRate : ' + str(AVG))
+	print('invest : ' + str([ STOCKS[i] for i in invest ]))
 	print('INV ReturnOfRate : ' + str(INV))
 	print('===============' + '======' + '===============')
 
