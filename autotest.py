be2c0cdc009b4f1c7c6696f7dd0535eff070601d
HsuBokai <exp2.718281@gmail.com>
2019-09-16 22:48:04 +0800

    readData and getData

--- autotest.py
+++ autotest.py
@@ -23,26 +23,47 @@ STOCKS = { \
 	'2889' : '國票金', \
 	'2890' : '永豐金', \
 	'2892' : '第一金', \
-	'5820' : '日盛金', \
+	#'5820' : '日盛金', \
 	#'5876' : '上海商銀', \
 	'5880' : '合庫金', \
 	}
 
 YEAR_COLUMN_MAP = { \
 	2018: 0, \
-	2017: 12, \
+	#2017: 12, \
 	#2016: 24, \
 	#2015: 36, \
 	}
 
-def getClose(date_str):
+def readData(f, index_str, column_str):
+	df = pd.read_csv(f)
+	if f[:7] == 'monthly':
+		df.columns = df.iloc[0]
+	df['4number'] = pd.to_numeric(df[index_str], errors='coerce')
+	df = df[df['4number'].notnull()]
+	return pd.to_numeric(df.set_index(index_str)[column_str], errors='coerce')
+
+def getData(folder, index_str, column_str):
+	d = { f[-6:]:readData(f,index_str,column_str) for f in gb.glob(folder+'/*') }
+	return pd.DataFrame(d).transpose().sort_index()
+
+def getMonth(m):
+	if (m > 12):
+		m-=12
+	return '{:02d}'.format(m)
+
+def getClose(year,m):
+	if (m > 12):
+		year+=1
+		m-=12
+	date_str = '{:04d}{:02d}'.format(year,m)
 	files = [ f for f in gb.glob('price/' + date_str + '*') ]
 	df = pd.read_csv(files[0])
 	#cond1 = pd.to_numeric(df['收盤價'], errors='coerce') < 10
-	conds = [ df['證券名稱'] == s for s in STOCKS.values() ]
+	conds = [ df['證券代號'] == s for s in STOCKS.keys() ]
 	cond2 = ft.reduce(lambda c1, c2: c1 | c2, conds[1:], conds[0])
 	df = df[cond2]
-	return pd.to_numeric(df.set_index('證券名稱')['收盤價'], errors='coerce')
+	return pd.to_numeric(df.set_index('證券代號')['收盤價'], errors='coerce')
 
 def getIndex(close):
 	equality = close.dropna(axis=1).mean(axis=1)
@@ -50,11 +71,11 @@ def getIndex(close):
 	return equality * range(1,equality.shape[0]+1) / equality.cumsum()
 
 def getReturn(close, choose):
-	cost_list = np.cumsum([ c.iloc[idx].sum()  for idx, c in enumerate(choose) ])
+	cost_list = np.cumsum([ close[c].iloc[idx].sum()  for idx, c in enumerate(choose) ])
 	#print(cost_list)
 	cost = pd.DataFrame(data=cost_list, index=close.index).sum(axis=1)
 	#print(cost)
-	equality_list = [ c.sum(axis=1) for idx, c in enumerate(choose) ]
+	equality_list = [ close[c].sum(axis=1) for idx, c in enumerate(choose) ]
 	for idx in range(0,close.shape[0]):
 		equality_list[idx].iloc[:idx] = 0
 	#print(equality_list[0])
@@ -63,21 +84,31 @@ def getReturn(close, choose):
 	return equality / cost
 
 def getRate(year):
-	close_dict = {}
-	for m in range(10,13):
-		date_str = '{:04d}{:02d}'.format(year,m)
-		close_dict['{:02d}'.format(m)] = getClose(date_str)
-	year+=1
-	for m in range(1,8):
-		date_str = '{:04d}{:02d}'.format(year,m)
-		close_dict['{:02d}'.format(m)] = getClose(date_str)
+	close_dict = { getMonth(m):getClose(year,m) for m in range(10,20) }
 	close = pd.DataFrame(close_dict).transpose()
 	#print(close)
 	#print((close.iloc[-1,:] / close).mean(axis=1).describe())
 
-	choose = [ close.loc[:, close.iloc[i] < 20 ] for i in range(0,close.shape[0]) ]
-	#choose = [ close for i in range(0,close.shape[0]) ]
-	#print([ c.iloc[0] for c in choose ])
+	revenue = getData('monthly','公司代號','當月營收')
+	#print(revenue)
+	revenue4 = revenue.rolling(4).mean()
+	#print(revenue4)
+	#print(revenue4.shift(-1) - revenue4)
+	#print(revenue)
+	eps = getData('income','公司代號','基本每股盈餘（元）')
+	#print(eps)
+	rev_cum = getData('income','公司代號','營業收入')
+	print(rev_cum[2330])
+	bps = getData('balance','公司代號','每股參考淨值')
+	#print(bps)
+	asset = getData('balance','公司代號','資產總計')
+	#print(asset)
+	capital = getData('balance','公司代號','權益總計')
+	#print(capital)
+
+	choose = [ close.loc[:, close.iloc[i] < 20 ].columns for i in range(0,close.shape[0]) ]
+	#choose = [ list(STOCKS.keys()) for i in range(0,close.shape[0]) ]
+	#print(choose)
 
 	index_rate = getIndex(close)
 	return_rate = getReturn(close, choose)
@@ -89,8 +120,8 @@ def getRate(year):
 
 def main():
 	data = pd.concat([ getRate(year) for year in YEAR_COLUMN_MAP.keys() ], axis=1)
-	data.plot()
-	plt.savefig('/mnt/rate.svg')
+	#data.plot()
+	#plt.savefig('/mnt/rate.svg')
 
 if __name__ == "__main__":
     main()
