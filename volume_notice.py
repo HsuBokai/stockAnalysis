b5369c15830fd837ac77c9e0869d10bc9b22d84a
HsuBokai <exp2.718281@gmail.com>
2019-11-30 10:16:02 +0800

    init volume notice

---
+++ volume_notice.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+
+import sys
+import pandas as pd
+import requests
+from io import StringIO
+import time
+import datetime as dt
+import notice
+
+def crawl_volume(date_str):
+	print(date_str)
+	r = requests.post('http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + date_str + '&type=ALL')
+	ret = pd.read_csv(StringIO("\n".join([i.translate({ord(c): None for c in ' '})
+					for i in r.text.split('\n')
+					if len(i.split('",')) == 17 and i[0] != '='])), header=0)
+	ret = ret.set_index('證券名稱')
+	ret['成交股數'] = ret['成交股數'].str.replace(',','')
+	return pd.to_numeric(ret['成交股數'], errors='coerce')
+
+def main(argv):
+	if 2 <= len(argv):
+		date_str = argv[1]
+	else:
+		now=dt.datetime.now()
+		date_str = '{:04d}{:02d}{:02d}'.format(now.year,now.month,now.day)
+	volume = crawl_volume(date_str)
+	results_dict = {}
+	msg = ''
+	for k,v in notice.NOTICE:
+		if volume[k] > v*1000:
+			msg += '{} 成交張數超過 {} 多 {} 張\n'.format(k,v,(volume[k]//1000-v))
+	if msg != '':
+		print(msg)
+	time.sleep(10)
+
+if __name__ == "__main__":
+    main(sys.argv)
