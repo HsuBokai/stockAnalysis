609e99209c0b834a63400284aad6c87dd7b2f410
HsuBoKai <exp2.718281@gmail.com>
2019-12-20 21:41:02 +0800

    fix mysql command fail

--- run_calculate.sh
+++ run_calculate.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-date_str=`date +'%Y%m%d'`; 
+date_str=`date +'%Y%m%d'`;
 
 docker run --rm -v  /volume1/homes/admin/stockAnalysis:/mnt -w /mnt --network eth0-macvlan stock:1 python calculate.py ${date_str}
-
-/usr/local/mariadb10/bin/mysql  -e "insert into my_db.stock_investment  (mDate, mPrice, mCost, mDiff, mPercent)  values `cat /volume1/homes/admin/stockAnalysis/stock_investment/${date_str}.txt`"
+result_file="/volume1/homes/admin/stockAnalysis/stock_investment/${date_str}.txt"
+if [ ! -f ${result_file} ]; then
+	echo "${result_file} not found!"
+	exit 0
+fi
+value=`cat ${result_file}`
+/usr/local/mariadb10/bin/mysql  -e "insert into my_db.stock_investment  (mDate, mPrice, mCost, mDiff, mPercent)  values ${value}"
+echo "${date_str} success!"
+exit 0
