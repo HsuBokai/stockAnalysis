d2c6577bc21235b95b97bb9c8e78393d661fba94
HsuBoKai <exp2.718281@gmail.com>
2020-07-19 18:25:09 +0800

    crawl_finance for now

--- crawl_finance.py
+++ crawl_finance.py
@@ -1,7 +1,8 @@
 #coding=utf-8
 
-import requests
+import sys
 import pandas as pd
+import requests
 import numpy as np
 import time
 import math
@@ -32,17 +33,20 @@ def financial_statement(year, season, type='綜合損益彙總表'):
 	time.sleep(10)
 	return df
 
-def main():
+def main(argv):
 	return
-	year=2017
-	for m in range(1,13):
-		date_str = '{:04d}{:02d}'.format(year,m)
-		print(date_str)
-		season = math.floor((m-1)/3)+1
-		financial_statement(year, season, '綜合損益彙總表').to_csv('/mnt/income/' + date_str)
-		financial_statement(year, season, '資產負債彙總表').to_csv('/mnt/balance/' + date_str)
-	#cond1 = df['基本每股盈餘（元）'] > 0.6
-	#print(df[cond1])
+	if 2 <= len(argv):
+		date_str = argv[1]
+		year = int(date_str[0:4])
+		m = int(date_str[4:6])
+	else:
+		year=2020
+		m = 1
+		date_str = '{:04d}{:02d}'.format(year, m)
+	print(date_str)
+	season = math.floor((m-1)/3)+1
+	financial_statement(year, season, '綜合損益彙總表').to_csv('/mnt/income/' + date_str)
+	financial_statement(year, season, '資產負債彙總表').to_csv('/mnt/balance/' + date_str)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
