5935ad52a2640cf2a80ca09789d51c41e2e9d427
HsuBoKai <exp2.718281@gmail.com>
2020-08-05 22:02:52 +0800

    get name and close from crawlPrice

--- get_close.py
+++ get_close.py
@@ -9,14 +9,10 @@ import numpy as np
 from datetime import datetime, timedelta
 
 
-def crawl_price(date_str):
+def crawlPrice(date_str):
 	r = requests.post('http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + date_str + '&type=ALL')
 	ret = pd.read_csv(StringIO("\n".join([i.translate({ord(c): None for c in ' '}) 
 					for i in r.text.split('\n') 
 					if len(i.split('",')) == 17 and i[0] != '='])), header=0)
 	time.sleep(10)
-	return pd.to_numeric(ret.set_index('證券代號')['收盤價'], errors='coerce')
-
-def getClose(date_str):
-	close_dict = { date_str[0:6] : crawl_price(date_str) }
-	return pd.DataFrame(close_dict).transpose()
+	return ret.set_index('證券代號')
