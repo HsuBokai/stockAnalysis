916adbf21e3f6d85d5e559a8dbaf971cfa12bded
HsuBoKai <exp2.718281@gmail.com>
2020-08-24 23:45:45 +0800

    volume notice threshold by year average v240
    a

--- volume_notice.py
+++ volume_notice.py
@@ -35,6 +35,28 @@ def my_send_email(content):
 		sys.exit(-2)
 	print('mail sent!')
 
+def get_yesterday_ma(date_str):
+	yesterday_ma = pd.read_csv('price_ma/' + date_str)
+	return yesterday_ma.set_index('name')
+
+def volume_info(today, yesterday_ma):
+	msg = ''
+	today_name_index = today.set_index('證券名稱')
+	today_name_index['volume'] = pd.to_numeric(today_name_index['成交股數'], errors='coerce')
+	today_name_index['change'] = pd.to_numeric(today_name_index['漲跌價差'], errors='coerce')
+	today_name_index['close'] = pd.to_numeric(today_name_index['收盤價'], errors='coerce')
+	today_name_index['percent'] = today_name_index['change'] / today_name_index['close'] * 100
+	for k,v in notice.NOTICE:
+		volume = today_name_index.loc[k, 'volume']
+		v = yesterday_ma.loc[k,'v240']
+		v = int(v*2)
+		if volume > v*1000:
+			rate = today_name_index.loc[k, 'percent'].round(3)
+			sign = today_name_index.loc[k, '漲跌(+/-)']
+			more = volume//1000 - v
+			msg += '{} 成交張數超過 {} 多 {} 張, 漲跌幅 {}{} %\n'.format(k,v,more,sign,rate)
+	return msg
+
 def main(argv):
 	if 2 <= len(argv):
 		date_str = argv[1]
@@ -48,20 +70,9 @@ def main(argv):
 	except:
 		my_send_email('Fail to crawl today price!')
 		sys.exit(-1)
-	today_name_index = today.set_index('證券名稱')
-	today_name_index['volume'] = pd.to_numeric(today_name_index['成交股數'], errors='coerce')
-	today_name_index['change'] = pd.to_numeric(today_name_index['漲跌價差'], errors='coerce')
-	today_name_index['close'] = pd.to_numeric(today_name_index['收盤價'], errors='coerce')
-	today_name_index['percent'] = today_name_index['change'] / today_name_index['close'] * 100
-	results_dict = {}
 	msg = ''
-	for k,v in notice.NOTICE:
-		volume = today_name_index.loc[k, 'volume']
-		if volume > v*1000:
-			rate = today_name_index.loc[k, 'percent'].round(3)
-			sign = today_name_index.loc[k, '漲跌(+/-)']
-			more = volume//1000 - v
-			msg += '{} 成交張數超過 {} 多 {} 張, 漲跌幅 {}{} %\n'.format(k,v,more,sign,rate)
+	yesterday_ma = get_yesterday_ma(date_str)
+	msg += volume_info(today, yesterday_ma)
 	msg += monthly_info()
 	try:
 		finance = getFinance()
